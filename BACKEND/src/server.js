a9debb947c160cfe3030ae306c1153c022146f74
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-12-17 20:51:26 +0100

    update server

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,19 +1,23 @@
-const express = require("express")
-const { notFoundHandler, badRequestHandler, genericErrorHandler } = require("./errorHeandlers")
-const cors = require("cors")
-const { join } = require("path")
-const listEndpoints = require("express-list-endpoints")
-const mongoose = require("mongoose")
-const staticFolderPath = join(__dirname, "../public")
-const profileRouter = require('./routes/profiles')
-const port = process.env.PORT
-const experienceRouter = require("./routes/experience")
-const post = require("./routes/post")
+const express = require("express");
+const {
+  notFoundHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./errorHeandlers");
+const cors = require("cors");
+const { join } = require("path");
+const listEndpoints = require("express-list-endpoints");
+const mongoose = require("mongoose");
+const staticFolderPath = join(__dirname, "../public");
+const profileRouter = require("./routes/profiles");
+const port = process.env.PORT;
+const experienceRouter = require("./routes/experience");
+const post = require("./routes/post");
 const authorizeRoutes = require("./routes/authorization");
-const cookieParser = require("cookie-parser")
-const server = express()
-const passport = require("passport")
-const oauth = require("../src/routes/profiles/oauth")
+const cookieParser = require("cookie-parser");
+const server = express();
+const passport = require("passport");
+const oauth = require("../src/routes/profiles/oauth");
 
 // const {
 //     notFoundHandler,
@@ -22,41 +26,39 @@ const oauth = require("../src/routes/profiles/oauth")
 //     newlyDefinedErrorHandler,
 //   } = require("./errorHandler")
 
-server.use(passport.initialize())
-server.use(express.static(staticFolderPath))
-server.use(express.json())
-server.use(cookieParser())
-server.use(cors())
+server.use(passport.initialize());
+server.use(express.static(staticFolderPath));
+server.use(express.json());
+server.use(cookieParser());
+server.use(cors());
 
-server.use("/post", post)
-server.use("/profiles", profileRouter)
-server.use("/experiences", experienceRouter)
+server.use("/post", post);
+server.use("/profiles", profileRouter);
+server.use("/experiences", experienceRouter);
 server.use("/user", authorizeRoutes);
 
-
-
-server.use(badRequestHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
-
+server.use(badRequestHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 // server.use(notFoundHandler)
 // server.use(badRequestHandler)
 // server.use(newlyDefinedErrorHandler)
 // server.use(otherGenericErrorHandler)
 
-console.log(listEndpoints(server))
+console.log(listEndpoints(server));
 
-
-
-mongoose.connect("mongodb+srv://eriseld:troy1894@cluster0.j7g0j.mongodb.net/linkedindb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(
-        server.listen(port || 3000, () => {
-            console.log("Running on port", port || 3000)
-        })
-    )
-    .catch((err) => console.log(err))
-
+mongoose
+  .connect(
+    "mongodb+srv://eriseld:troy1894@cluster0.j7g0j.mongodb.net/linkedindb",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(
+    server.listen(port || 3000, () => {
+      console.log("Running on port", port || 3000);
+    })
+  )
+  .catch((err) => console.log(err));
